df133ec85dceffb60c897f3fce921ef8fce872b4
Ohad Mizrachi <ohad24@gmail.com>
2021-10-20 21:32:28 +0300

    update create user method

--- src/api/endpoints/users.py
+++ src/api/endpoints/users.py
@@ -12,16 +12,16 @@ router = APIRouter()
 async def read_users(
     db: MongoClient = Depends(db.get_db), token: str = Depends(oauth2_scheme)
 ):
-    users = [user for user in db.users.find({})]
+    users = list(db.users.find({}))
     print(users)
     return users
 
 
-@router.post("/", response_model=user_model.User)
+@router.post("/", response_model=user_model.UserCreateOut)
 async def create_user(
     *,
     db: MongoClient = Depends(db.get_db),
-    user_in: user_model.UserCreate,
+    user_in: user_model.UserCreateIn,
     # current_user: user_model.User = Depends(deps.get_current_active_superuser),
 ) -> Any:
     user = db.users.find_one({"username": user_in.username})
@@ -30,23 +30,7 @@ async def create_user(
             status_code=400,
             detail="The user with this username already exists in the system.",
         )
-    # user = crud.user.create(db, obj_in=user_in)
     user_in.password = get_password_hash(user_in.password)
     inserted_id = db.users.insert_one(user_in.dict()).inserted_id
     user = db.users.find_one({"_id": inserted_id})
-    print(user)
     return user
-
-
-# @router.get("/", response_model=List[schemas.User])
-# def read_users(
-#     db: Session = Depends(deps.get_db),
-#     skip: int = 0,
-#     limit: int = 100,
-#     current_user: models.User = Depends(deps.get_current_active_superuser),
-# ) -> Any:
-#     """
-#     Retrieve users.
-#     """
-#     users = crud.user.get_multi(db, skip=skip, limit=limit)
-#     return users
--- src/api/models/base.py
+++ src/api/models/base.py
@@ -5,19 +5,6 @@ from datetime import datetime
 from bson import ObjectId
 
 
-class DBBaseModel(BaseModel):
-    # * https://pydantic-docs.helpmanual.io/usage/model_config/#change-behaviour-globally
-
-    _db_id: ObjectId = PrivateAttr(Field(alias="_id", title="Mongo object id"))
-    # create_date: datetime = Field(
-    #     default_factory=datetime.utcnow, hidden_from_schema=True, title="Creation date"
-    # )
-
-    class Config:
-        arbitrary_types_allowed = True
-        json_encoders = {ObjectId: str, Decimal128: str}
-
-
 class PyObjectId(ObjectId):
     @classmethod
     def __get_validators__(cls):
@@ -32,3 +19,12 @@ class PyObjectId(ObjectId):
     @classmethod
     def __modify_schema__(cls, field_schema):
         field_schema.update(type="string")
+
+
+class DBBaseModel(BaseModel):
+    # * https://pydantic-docs.helpmanual.io/usage/model_config/#change-behaviour-globally
+    _id: PyObjectId = Field(alias="_id", default_factory=PyObjectId)
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str, Decimal128: str}
