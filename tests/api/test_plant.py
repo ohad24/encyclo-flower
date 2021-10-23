a7f0b9adea6f2bb4c2e351807862af7430e5deba
Ohad Mizrachi <ohad24@gmail.com>
2021-10-23 14:34:26 +0300

    simple search test

--- tests/api/test_plant.py
+++ tests/api/test_plant.py
@@ -19,7 +19,12 @@ class TestSimpleSearch:
             params={"name_text": "not_a_real_name"},
         )
         assert response.status_code == 200
-        assert response.json() == {"total": 0, "plants": []}
+        assert response.json() == {
+            "total": 0,
+            "plants": [],
+            "pages": 1,
+            "current_page": 1,
+        }
 
     def test_simple_search(self):
         response = client.get(
@@ -38,18 +43,42 @@ class TestSimpleSearch:
         )
         assert response.status_code == 200
         assert response.json()["total"] == 23
-        assert len(response.json()["plants"]) == 10
+        assert len(response.json()["plants"]) == 23
 
-        for page in range(math.floor(response.json()["total"] / 10)):
+    def test_simple_search_with_multi_page(self):
+        params = {"name_text": "מצוי", "season_num": "3"}
+        response = client.get(
+            "/api/v1/plants/simple_search", headers=pytest.headers, params=params
+        )
+        assert response.status_code == 200
+        assert response.json()["total"] == 98
+
+        for page in range(response.json()["pages"]):
             response = client.get(
                 "/api/v1/plants/simple_search",
                 headers=pytest.headers,
-                params=params | {"page": page},
+                params=params | {"page": page + 1},
             )
             assert response.status_code == 200
-            assert response.json()["total"] == 23
+            assert response.json()["total"] == 98
             assert (
-                len(response.json()["plants"]) == 10
-                if page < 2
-                else response.json()["total"] % 10
+                len(response.json()["plants"]) == 30
+                if response.json()["current_page"] < response.json()["pages"]
+                else response.json()["total"] % 30
             )
+
+    def test_simple_search_page_out_of_range(self):
+        params = {"name_text": "מצוי", "season_num": "3"}
+        response = client.get(
+            "/api/v1/plants/simple_search", headers=pytest.headers, params=params
+        )
+        assert response.status_code == 200
+        assert response.json()["total"] == 98
+
+        response = client.get(
+            "/api/v1/plants/simple_search",
+            headers=pytest.headers,
+            params=params | {"page": response.json()["pages"] + 1},
+        )
+        assert response.status_code == 400
+        assert response.json() == {"detail": "page number out of range"}
