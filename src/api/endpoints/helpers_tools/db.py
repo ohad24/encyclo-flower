498b62c869ef4c0b65d20c926daf8043b2ff3767
Ofer Sadan <ofersadan85@gmail.com>
2022-03-06 18:33:11 +0000

    refactor search query based on search_input.dict

--- src/api/endpoints/helpers_tools/db.py
+++ src/api/endpoints/helpers_tools/db.py
@@ -24,34 +24,14 @@ def prepare_query_plant_name_text(name_text):
 
 
 def prepare_search_query(search_input: SearchIn) -> dict:
-    query_and = []
-    if search_input.name_text:
-        query_and.append(prepare_query_plant_name_text(search_input.name_text))
-    if search_input.flowering_seasons:
-        query_and.append({"flowering_seasons": {"$in": search_input.flowering_seasons}})
-    if search_input.colors:
-        query_and.append({"colors": {"$in": search_input.colors}})
-    if search_input.location_names:
-        for location_name in search_input.location_names:
-            query_and.append({"locations.location_name": location_name})
-    if search_input.petals:
-        query_and.append({"petal_num": {"$in": search_input.petals}})
-    if search_input.life_forms:
-        query_and.append({"life_forms": {"$in": search_input.life_forms}})
-    if search_input.habitats:
-        query_and.append({"habitats": {"$in": search_input.habitats}})
-    if search_input.stem_shapes:
-        query_and.append({"stem_shapes": {"$in": search_input.stem_shapes}})
-    if search_input.spine:
-        query_and.append({"spine": {"$in": search_input.spine}})
-    if search_input.red:
-        query_and.append({"red": search_input.red})
-    if search_input.invasive:
-        query_and.append({"invasive": search_input.invasive})
-    if search_input.danger:
-        query_and.append({"danger": search_input.danger})
-    if search_input.rare:
-        query_and.append({"rare": search_input.rare})
+    query_and = [prepare_query_plant_name_text(search_input.name_text)] if search_input.name_text else []
+    for field, value in search_input.dict(exclude_none=True, exclude_unset=True).items():
+        if field == 'location_names':
+            query_and += [{"locations.location_name": location_name} for location_name in value]
+        elif isinstance(value, list):
+            query_and += [{field: {'$in': value}}]
+        elif isinstance(value, bool):
+            query_and += [{field: value}]
 
     if not query_and:
         raise HTTPException(
