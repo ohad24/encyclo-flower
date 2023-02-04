7da52f2a7369c271c99d64af6315fdaf4c51cc79
Ohad Mizrachi <ohad24@gmail.com>
2023-02-04 13:50:24 +0200

    update gh action - build and test

--- tests/image_recognition/test_cloud_run.py
+++ tests/image_recognition/test_cloud_run.py
@@ -7,10 +7,10 @@ from pathlib import Path
 
 
 """
-    # * Based on https://github.com/GoogleCloudPlatform/python-docs-samples/blob/HEAD/auth/service-to-service/auth.py
-    This script run on GH Actions and test the Cloud Run service after build.
-    You can run this locally with
-    pytest tests/image_recognition/test_cloud_run.py --url=$( gcloud run services describe detect-tmp --region us-central1 --format=json | jq .status.url --raw-output) --google-cred-file-location=src/api/google_cred.json
+# * Based on https://github.com/GoogleCloudPlatform/python-docs-samples/blob/HEAD/auth/service-to-service/auth.py
+This script run on GH Actions and test the Cloud Run service after build.
+You can run this locally with
+pytest tests/image_recognition/test_cloud_run.py --url=$( gcloud run services describe detect-tmp --region us-central1 --format=json | jq .status.url --raw-output) --google-cred-file-location=src/api/google_cred.json
 """
 
 
@@ -65,20 +65,20 @@ def files():
     return {"file": open("./tests/assets/images/58NY77V207Q7H06.jpg", "rb")}
 
 
-class TestCloudRunDetectService:
-    def test_unauthorized(self, id_token, files, service_url):
-        headers = {"Authorization": f"Bearer {id_token}1"}  # * Invalid token
-        response = requests.post(service_url, headers=headers, files=files)
-        assert response.status_code == 401
+def test_unauthorized(id_token, files, service_url):
+    headers = {"Authorization": f"Bearer {id_token}1"}  # * Invalid token
+    response = requests.post(service_url, headers=headers, files=files)
+    assert response.status_code == 401
 
-    def test_authorized(self, id_token, files, service_url):
-        headers = {"Authorization": f"Bearer {id_token}"}
-        response = requests.post(service_url, headers=headers, files=files)
-        assert response.status_code == 200, response.text
-        assert response.json() == [
-            {"class_name": "Erodium gruinum", "score": 0.13281},
-            {"class_name": "Ipomoea indica", "score": 0.05469},
-            {"class_name": "Solanum laciniatum", "score": 0.04688},
-            {"class_name": "Nicandra physalodes", "score": 0.04297},
-            {"class_name": "Hibiscus trionum", "score": 0.02344},
-        ]
+
+def test_authorized(id_token, files, service_url):
+    headers = {"Authorization": f"Bearer {id_token}"}
+    response = requests.post(service_url, headers=headers, files=files)
+    assert response.status_code == 200, response.text
+    assert response.json() == [
+        {"class_name": "Erodium gruinum", "score": 0.13281},
+        {"class_name": "Ipomoea indica", "score": 0.05469},
+        {"class_name": "Solanum laciniatum", "score": 0.04688},
+        {"class_name": "Nicandra physalodes", "score": 0.04297},
+        {"class_name": "Hibiscus trionum", "score": 0.02344},
+    ]
